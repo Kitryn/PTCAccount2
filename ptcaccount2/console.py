ddbfdbb54c6b682c83c44b6ceed70692104abb32
Kit <snowfennard@gmail.com>
2016-08-18 06:42:45 +0800

    Fixed bug if email field is empty. Close #28

--- ptcaccount2/console.py
+++ ptcaccount2/console.py
@@ -57,7 +57,7 @@ def entry():
     try:
         if args.multiple > 1 and args.username is not None:
             raise ValueError("Username cannot be set if --multiple is greater than 1!")
-        if len(args.email) > 75:
+        if args.email is not None and len(args.email) > 75:
             raise ValueError("Email cannot be longer than 75 characters!")
 
         if args.multiple > 1 and args.email is not None:
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 DIST_NAME = 'PTCAccount2'
-VERSION = 'v2.0.7'
+VERSION = 'v2.0.8'
 AUTHOR = 'Kitryn'
 EMAIL = 'snowfennard@gmail.com'
 GITHUB_USER = 'Kitryn'
